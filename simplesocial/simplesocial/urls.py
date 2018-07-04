541cf8a6198b5c40e57329ab02d104ffb327ac1b
Ted Dunn <tldunn451@gmail.com>
2018-07-04 08:36:47 -0400

    add views/routes to thank new users for signing up and visiting the site

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -20,6 +20,8 @@ from . import views
 urlpatterns = [
     url('^$', views.HomePage.as_view(), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^test/$', views.TestPage.as_view(), name='test'),
+    url(r'^thanks/$', views.ThanksPage.as_view(), name='thanks'),
 ]
